e9eff66dfe4579d9bef408a6e55fec21d76431fe
Gianluca-Basili <gianluc.basili@gmail.com>
2023-07-24 13:33:58 +0200

    add dc logo and folder img

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,19 @@
+<header>
+    <div class="heder-top">
+
+    </div>
+    <div class="header-bottom">
+        <div class="container">
+            <div class="navbar">
+                <div class="logo">
+                    <a href="{{url('/')}}">
+                        <img src="{{Vite::asset('/resources/img/dc-logo.png')}}">
+                    </a>
+                </div>
+                <nav class="list-unstyled">
+                    
+                </nav>
+            </div>
+        </div>
+    </div>
+</header>
