d6a153e636aee97b1826a5b445280888a5f3200e
Gianluca-Basili <gianluc.basili@gmail.com>
2023-07-24 15:27:16 +0200

    add img and title img

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -21,15 +21,25 @@
         <div class="jumbotron">
         <img src="{{Vite::asset('/resources/img/jumbotron.jpg')}}" class="img-jumbotron">
         </div>
-        <div class="container">
-            <div class="row">
-                @foreach($comics as $comic)
-                <div class="col-12 col-lg-2">
-                    <img src=" {{$comic['thumb']}}">
-                    {{$comic['series']}}
+        <div class="full-container bg-dark">
+            
+            <div class="container">
+                <div class="row">
+                    <div>
+                        <h5 class="my-3">CURRENT SERIES</h5>
+                    </div>
+                    @foreach($comics as $comic)
+                    <div class="col-12 col-lg-2 my-5 ">
+                        <img src=" {{$comic['thumb']}}" class="thumb">
+                        <h6 class="my-3">{{$comic['series']}}</h6>
+                        
+                    </div>
+                    @endforeach
+                    <div class="d-flex justify-content-center">
+                        <button type="button" class="btn btn-primary button-load-more">LOAD MORE</button>
 
+                    </div>
                 </div>
-                @endforeach
             </div>
         </div>
     </main>
